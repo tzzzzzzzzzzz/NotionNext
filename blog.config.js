42bb7c1e815990da743ac989308706911ed4febd
Tianzhe <48273400+tzzzzzzzzzzz@users.noreply.github.com>
2024-07-08 11:16:23 +0800

    Update blog.config.js
    修改欢迎语，修改网站关键字

--- blog.config.js
+++ blog.config.js
@@ -20,14 +20,14 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '世间好景览无数，不如知你胜此时。',
+    '林花谢了春红，太匆匆。',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'TianXiYou', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || ' 社畜、京吹、LOLer、编程猿、卷毛', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tianxiyoublog.vip.cpolar.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, 杂想, 漫谈', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.tianxiyou.site', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '电子折腾, 阅读, 杂想, 漫谈', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'tz1063659575@gmail.com', // 邮箱地址 例如mail@tangly1024.com
